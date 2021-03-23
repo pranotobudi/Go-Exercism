57a360380e9076c1faa981de24b5e0aed913fefe
Pranoto Budi <pranotobudi.s@gmail.com>
2021-03-23 10:30:29 +0700

    triangle problem

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,28 +1,38 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package triangle will handle functions related to triangle
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+// Kind is an integer type which is correspondence to Nat, Equ, Iso, Sca constants
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// We'll use integer for the following identifiers used by the test program.
+	NaT = 0 // not a triangle
+	Equ = 1 // equilateral
+	Iso = 2 // isosceles
+	Sca = 3 // scalene
 )
 
-// KindFromSides should have a comment documenting it.
+// KindFromSides receive 3 float64 number and return Kind type to check whether it is a triangle or not
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	var k Kind
+	if a <= 0 || b <= 0 || c <= 0 {
+		k = NaT
+	} else if a+b < c || b+c < a || a+c < b {
+		k = NaT
+	} else if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		k = NaT
+	} else if math.IsInf(a, 1) || math.IsInf(b, 1) || math.IsInf(c, 1) {
+		k = NaT
+	} else if math.IsInf(a, -1) || math.IsInf(b, -1) || math.IsInf(c, -1) {
+		k = NaT
+	} else if a == b && b == c && a == c {
+		k = Equ
+	} else if a == b || b == c || a == c {
+		k = Iso
+	} else if a != b && b != c && a != c {
+		k = Sca
+	}
 	return k
 }
